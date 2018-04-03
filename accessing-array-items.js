5cdeefee5707ce6cd75322777377b97cfa359fe2
Craig Pounds <nweaver@windstream.net>
2018-04-03 11:30:59 -0500

    third exercise

---
+++ accessing-array-items.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function accessFirstItem(array) {
+  // your code goes here
+  return array[0];
+}
+
+function accessThirdItem(array) {
+  // your code goes here
+  return array[2];
+}
