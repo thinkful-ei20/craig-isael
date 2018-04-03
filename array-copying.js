74da90580e2e41cb2b0d7ce88459d046a155603d
Craig Pounds <nweaver@windstream.net>
2018-04-03 11:39:34 -0500

    answered fifth drill

---
+++ array-copying.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function firstFourItems(array) {
+  // your code goes here
+  return array.slice(0, 4);
+}
+
+function lastThreeItems(array) {
+  // your code goes here
+  return array.slice(-3);
+}
