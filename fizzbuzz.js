4445dea67d7be02fbd3c657381ad9b74b5fcd415
Craig Pounds <nweaver@windstream.net>
2018-04-03 19:18:38 -0500

    reworked fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,18 +1,39 @@
 'use strict';
 
+// function fizzBuzz(countTo) {
+//   const results = [];
+//   for (let i = 1; i <= countTo; i++) {
+//     if (i % 15 === 0) {
+//       results.push('fizzbuzz');
+//     } else if (i % 5 === 0) {
+//       results.push('buzz');
+//     } else if (i % 3 === 0) {
+//       results.push('fizz');
+//     } else {
+//       results.push(i);
+//     }
+//   }
+//   return results;
+// }
+
 function fizzBuzz(countTo) {
-  const results = [];
+  
+  let arr = [];
+
   for (let i = 1; i <= countTo; i++) {
-    if (i % 15 === 0) {
-      results.push('fizzbuzz');
-    } else if (i % 5 === 0) {
-      results.push('buzz');
-    } else if (i % 3 === 0) {
-      results.push('fizz');
-    } else {
-      results.push(i);
+    let results = '';
+    if (i % 3 === 0) {
+      results += 'fizz';
     }
+    if (i % 5 === 0) {
+      results += 'buzz';
+    }
+    if (results === '') {
+      results = i;
+    }
+
+    arr.push(results);
   }
-  return results;
+  return arr;
 }
 console.log(fizzBuzz(100));
